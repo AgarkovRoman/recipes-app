5236d3f4fc0f6f1546ce75745dbb02436a952ae5
Roman Agarkov <agarkovromachka@mail.ru>
2020-07-17 00:32:39 +0300

    add search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,34 @@
-import axios from 'axios'
+import Search from './models/Search'
 
-async function getResults(query) {
-    try {
-        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-        const recipes = res.data.recipes
-        console.log(recipes)
-    } catch (error) {
-        console.log(error)
+/***Global state 
+* - Search object
+* - Current recipe object
+* - Shoping list object
+* - Liked recipes
+*/
+
+const state = {}
+
+
+const controlSearch = async () => {
+    //1 get query from the view
+    const query = 'pizza'
+
+    if (query) {
+        //2 new earch object and add to state
+        state.search = new Search(query)
+        //3 prepare UI for results
+
+        //4 search for recipes
+        await state.search.getResults();
+        //5 render resalts on UI
+        console.log(state.search.result)
     }
-    // // console.log(res)
-    // console.log(recipes)
 }
-getResults('pizza')
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
+// search.getResults();
