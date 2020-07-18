fa21311fdf5f3e3fce67bfde9c536e3ea86b26e6
Roman Agarkov <agarkovromachka@mail.ru>
2020-07-18 14:48:20 +0300

    add loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/SearchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /***Global state 
 * - Search object
@@ -22,12 +22,14 @@ const controlSearch = async () => {
 
         //3 prepare UI for results
         searchView.clearInput();
-        searchView.clearResults()
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         //4 search for recipes
         await state.search.getResults();
 
         //5 render resalts on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,24 @@
 export const elements = {
     searchInput: document.querySelector('.search__field'),
     searchForm: document.querySelector('.search'),
-    searchResList: document.querySelector('.results__list')
+    searchRes: document.querySelector('.results'),
+    searchResList: document.querySelector('.results__list'),
+};
+
+export const elementStrings = {
+    loader: 'loader'
+}
+
+export const renderLoader = parent => {
+    const loader = `
+    <div class="${elementStrings.loader}">
+        <div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader);
+}
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`)
+    if (loader) loader.parentElement.removeChild(loader);
 }
